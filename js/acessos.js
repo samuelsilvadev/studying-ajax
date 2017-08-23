62c4b92ff0f97ebd5315857a72c50d35be9d485e
samuelsilva <samuelsilvawb@gmail.com>
2017-08-23 20:43:18 -0300

    Inserindo key do new york times

--- js/acessos.js
+++ js/acessos.js
@@ -3,7 +3,14 @@ const keys = {
 	streeView : {
 		API_KEY : "AIzaSyCA5GyiPvbe6fDXjr_nTudNNRILDnByVCA",
 		url 	: function(adress, city){
+			console.log(adress, city);
 			return `https://maps.googleapis.com/maps/api/streetview?size=1366x768&location=${adress}${city}&heading=151.78&pitch=-0.76&key=${this.API_KEY}`;
+		}
+	},
+	newYorkTimes : {
+		API_KEY : "5af775806b0947b0b794a71a41125749",
+		url 	: function(){
+			return ``;
 		}
 	}
 }
