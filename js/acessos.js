65d325200963e9e40663e39b41b1962f80a04ff6
samuel silva <samuelsilvawb@gmail.com>
2017-10-11 20:47:47 -0300

    Refatorando arquivos, fazendo get em news

--- js/acessos.js
+++ js/acessos.js
@@ -2,15 +2,14 @@
 const keys = {
 	streeView : {
 		API_KEY : "AIzaSyCA5GyiPvbe6fDXjr_nTudNNRILDnByVCA",
-		url 	: function(adress, city){
-			console.log(adress, city);
+		url 	: function(adress, city){		
 			return `https://maps.googleapis.com/maps/api/streetview?size=1366x768&location=${adress}${city}&heading=151.78&pitch=-0.76&key=${this.API_KEY}`;
 		}
 	},
 	newYorkTimes : {
 		API_KEY : "5af775806b0947b0b794a71a41125749",
 		url 	: function(){
-			return `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${this.API_KEY}`;
+			return `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${this.API_KEY}`;		
 		}
 	}
 }
--- js/handlerEvents.js
+++ js/handlerEvents.js
@@ -1,25 +1,32 @@
 
 let searchImage = null;
 
+const elements = {
+	txtAdress : document.querySelector('#txtAdress'),
+	txtCity : document.querySelector('#txtCity'),
+	frmSearchAdress : document.querySelector('#frmSearchAdress'),
+	imgNewCity : document.querySelector('img')
+}
+
 addEventListener('load', () => {
 	searchImage = new SearchImage(
-		document.querySelector('#txtAdress'),
-		document.querySelector('#txtCity')
+		elements.txtAdress,
+		elements.txtCity
 	);
 });
 
-document.querySelector('#frmSearchAdress')
-	.addEventListener('submit', e => {
+elements.frmSearchAdress.addEventListener('submit', e => {
 	e.preventDefault();
 	console.log(searchImage.adress.value);
 	console.log(searchImage.city.value);
 	console.log(keys.streeView.url());
-	document.querySelector('img').src = keys.streeView.url(searchImage.adress.value, searchImage.city.value);
+	elements.imgNewCity.src = keys.streeView.url(searchImage.adress.value, searchImage.city.value);
+	getNews();
 });
 
 (function agendaTask(){
 	setTimeout(() => {
-			
+	
 	}, 5000);
 });
 
