9f141f2186711d78f04b8c83a252ab3ec5037ad9
samuel silva <samuelsilvawb@gmail.com>
2017-10-24 22:50:54 -0300

    feat : Adicionar dinamicamente as news da região buscada no index
    - Trocado article pela tag div
    - adicionado classe para indicar quem é o componente que recebera os itens
    - adicionado itens via js
    
    Samuel Silva

--- js/handlerEvents.js
+++ js/handlerEvents.js
@@ -5,7 +5,8 @@ const elements = {
 	txtAdress : document.querySelector('#txtAdress'),
 	txtCity : document.querySelector('#txtCity'),
 	frmSearchAdress : document.querySelector('#frmSearchAdress'),
-	imgNewCity : document.querySelector('img')
+	imgNewCity : document.querySelector('img'),
+	articleItens: document.querySelector('.articles__itens')
 }
 
 addEventListener('load', () => {
@@ -18,5 +19,27 @@ addEventListener('load', () => {
 elements.frmSearchAdress.addEventListener('submit', e => {
 	e.preventDefault();
 	elements.imgNewCity.src = keys.streeView.url(searchImage.adress.value, searchImage.city.value);
-	getNews(searchImage.adress.value, searchImage.city.value);
+	getNews(searchImage.adress.value, searchImage.city.value).then(succ => buildArticles(succ));
 });
+
+function buildArticles(articles){
+	console.log(articles.response.docs);
+	let finalContentHTML = '';
+
+	const everyArticles = articles.response.docs.map(element => {
+		
+		return`	<div class="col s4 m4">
+					<div class="card blue-grey darken-1">
+						<div class="card-content white-text">
+							<span class="card-title">${element.headline.main}</span>
+								<p>${element.snippet}</p>
+						</div>
+						<div class="card-action">
+							<a href="${element.web_url}">Url</a>									
+						</div>
+					</div>
+				</div>`;		
+	}).join('');
+
+	elements.articleItens.innerHTML = everyArticles;
+}
